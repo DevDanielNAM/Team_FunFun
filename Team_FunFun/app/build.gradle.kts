382b1d737a39445ef7fbd83c00142e08933e817d
DevDanielNAM <danielnam426@gmail.com>
2023-11-12 04:54:49 +0900

    Bottom Navigation Bar 생성, 초기화된 Fragment Page 생성, 아이콘 drawable에 추가, 최소 api 28

--- Team_FunFun/app/build.gradle.kts
+++ Team_FunFun/app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.team_funfun"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.team_funfun"
-        minSdk = 24
-        targetSdk = 33
+        minSdk = 28
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -33,8 +33,10 @@ android {
 
 dependencies {
 
+    implementation ("com.google.android.material:material:1.10.0")
+    implementation ("androidx.navigation:navigation-fragment:2.7.5")
+    implementation ("androidx.navigation:navigation-ui:2.7.5")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
