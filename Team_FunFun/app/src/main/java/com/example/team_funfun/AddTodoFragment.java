a133da4ed2ebe64ce593bbb8e1bfd90d38dbfcaf
DevDanielNAM <danielnam426@gmail.com>
2023-11-18 05:09:42 +0900

    DB 완성, todo 추가, 수정 페이지 레이아웃 어느정도 완성, 카테고리 모드 진행 중

--- Team_FunFun/app/src/main/java/com/example/team_funfun/AddTodoFragment.java
+++ Team_FunFun/app/src/main/java/com/example/team_funfun/AddTodoFragment.java
@@ -2,9 +2,15 @@ package com.example.team_funfun;
 
 import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 
 import androidx.appcompat.widget.AppCompatButton;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -18,7 +24,6 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.sql.Date;
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
@@ -36,6 +41,7 @@ public class AddTodoFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_add_todo, container, false);
 
         MainActivity mainActivity = (MainActivity)getActivity();
@@ -57,7 +63,8 @@ public class AddTodoFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 if(categoryInput.getText().toString().compareTo("") != 0) {
-                    mainActivity.insertCategoryData(categoryInput.getText().toString());
+                    mainActivity.insertCategoryData(categoryInput.getText().toString(),"#BFFF4141");
+                    categoryInput.setText("");
                     renderCategoryBtn(mainActivity);
                 }
             }
@@ -76,16 +83,26 @@ public class AddTodoFragment extends Fragment {
         addTodo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(todoInput.getText().toString().compareTo("") != 0
-                && dateInput.getText().toString().compareTo("") != 0
-                && clickedCategory.compareTo("") != 0) {
+                if(!todoInput.getText().toString().equals("")
+                && !dateInput.getText().toString().equals("")
+                && !clickedCategory.equals("")) {
                     mainActivity.insertTodoData(todoInput.getText().toString(),
                             Date.valueOf(dateInput.getText().toString()),
-                            clickedCategory,
-                            0);
+                            0,
+                            clickedCategory);
                 }
             }
         });
+
+//        mainActivity.updateTodoData(todoInput.getText().toString(),
+//                Date.valueOf(dateInput.getText().toString()),
+//                0,
+//                clickedCategory);
+
+//        List<String[]> getTodoData = mainActivity.getTodoData();
+//        for(String[] todoData : getTodoData)
+//            System.out.println(todoData[0] + todoData[1] + todoData[2] + todoData[3] + todoData[4]);
+
         return rootView;
     }
 
@@ -111,36 +128,89 @@ public class AddTodoFragment extends Fragment {
         });
     }
 
-    private void addCategoryBtn(String category) {
+    // 카테고리 버튼 생성
+    private void addCategoryBtn(String category, String color) {
         AppCompatButton newCategoryBtn = new AppCompatButton(getContext());
+        int textColor = getContrastColor(color);
+        int clickedColor = adjustColor(Color.parseColor(color), 0.8f);
+
+        Typeface typeface = ResourcesCompat.getFont(getContext(), R.font.ibm_plex_sans_kr_light);
+        newCategoryBtn.setTypeface(typeface);
         newCategoryBtn.setText(category);
+        newCategoryBtn.setTextSize(16);
+        newCategoryBtn.setTextColor(textColor);
+        newCategoryBtn.setHeight(30);
         newCategoryBtn.setAllCaps(false);
-        newCategoryBtn.setBackgroundResource(R.drawable.unclicked_category_button);
+
+        // 전달받은 color를 unclicked 상태의 버튼에 적용
+        Drawable unclickedDrawable = ContextCompat.getDrawable(getContext(), R.drawable.unclicked_category_button);
+        GradientDrawable gradientUnclickedDrawable = (GradientDrawable) unclickedDrawable;
+        if (gradientUnclickedDrawable != null) {
+            gradientUnclickedDrawable.setColor(Color.parseColor(color));
+        }
+
+        // 전달받은 color를 clicked 상태의 버튼에 적용
+        Drawable clickedDrawable = ContextCompat.getDrawable(getContext(), R.drawable.clicked_category_button);
+        GradientDrawable gradientClickedDrawable = (GradientDrawable) clickedDrawable;
+        if (gradientClickedDrawable != null) {
+            gradientClickedDrawable.setColor(clickedColor);
+        }
+
+        newCategoryBtn.setBackgroundDrawable(unclickedDrawable);
+
         newCategoryBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(isClicked) {
-                    newCategoryBtn.setBackgroundResource(R.drawable.clicked_category_button);
+                    newCategoryBtn.setBackgroundDrawable(clickedDrawable);
                     clickedCategory = newCategoryBtn.getText().toString();
                     isClicked = false;
                 } else {
-                    newCategoryBtn.setBackgroundResource(R.drawable.unclicked_category_button);
+                    newCategoryBtn.setBackgroundDrawable(unclickedDrawable);
                     isClicked = true;
                 }
             }
         });
-        categoryContainer.addView(newCategoryBtn, new LinearLayout.LayoutParams(
+
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
                 ViewGroup.LayoutParams.WRAP_CONTENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT));
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        int marginInPixels = (int) getResources().getDimension(R.dimen.margin);
+        layoutParams.setMargins(marginInPixels, marginInPixels, marginInPixels, marginInPixels);
+
+        categoryContainer.addView(newCategoryBtn, layoutParams);
     }
 
+    private int adjustColor(int color, float factor) {
+        float[] hsv = new float[3];
+        Color.colorToHSV(color, hsv);
+        hsv[2] *= factor; // adjust brightness
+        return Color.HSVToColor(hsv);
+    }
+
+    private int getContrastColor(String backgroundColor) {
+        int backgroundRgb = Color.parseColor(backgroundColor);
+        int red = Color.red(backgroundRgb);
+        int green = Color.green(backgroundRgb);
+        int blue = Color.blue(backgroundRgb);
+
+        // Calculate relative luminance using the formula
+        double luminance = (0.299 * red + 0.587 * green + 0.114 * blue) / 255.0;
+
+        // Choose black or white text based on the luminance value
+        return luminance > 0.5 ? Color.BLACK : Color.WHITE;
+    }
+
+    // DB에서 카테고리 항목을 조회해서 카테고리 버튼을 렌더
     private void renderCategoryBtn(MainActivity mainActivity) {
         categoryContainer.removeAllViews();
-        List<String> getCategoryList = new ArrayList<>();
-        getCategoryList = mainActivity.getCategoryData("Category");
-        int count = mainActivity.getCategoryCount("Category");
-        for(int i = 0; i<count; i++) {
-            addCategoryBtn(getCategoryList.get(i));
+        List<String[]> getCategoryList = mainActivity.getCategoryData();
+        if (getCategoryList != null) {
+            for (String[] categoryData : getCategoryList) {
+                addCategoryBtn(categoryData[0], categoryData[1]);
+            }
         }
     }
+
 }
--- Team_FunFun/app/src/main/java/com/example/team_funfun/MainActivity.java
+++ Team_FunFun/app/src/main/java/com/example/team_funfun/MainActivity.java
@@ -18,21 +18,17 @@ public class MainActivity extends AppCompatActivity {
     HomeFragment homeFragment;
     CalendarFragment calendarFragment;
     CategoryFragment categoryFragment;
-    SQLiteDatabase dbTodo;
-    SQLiteDatabase dbCategory;
+    SQLiteDatabase todoDb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String databaseTodo = "Todo";
-        String databaseCategory = "Category";
+        String dbName = "todoDb";
+        openDatabase(dbName);
+        createTable();
 
-        openDatabase(databaseTodo, databaseCategory);
-        createTable(databaseTodo, databaseCategory);
-//        deleteDatabase(databaseTodo);
-//        deleteDatabase(databaseCategory);
         homeFragment = new HomeFragment();
         calendarFragment = new CalendarFragment();
         categoryFragment = new CategoryFragment();
@@ -65,131 +61,198 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void openDatabase(String databaseTodo, String databaseCategory){
+    public void openDatabase(String dbName){
         println("openDatabase() 호출됨");
-        dbTodo = openOrCreateDatabase(databaseTodo, MODE_PRIVATE,null) ; //보안때문에 요즘은 대부분 PRIVATE사용, SQLiteDatabase객체가 반환됨
-        if(dbTodo !=null){
+        todoDb = openOrCreateDatabase(dbName, MODE_PRIVATE,null) ; //보안때문에 요즘은 대부분 PRIVATE사용, SQLiteDatabase객체가 반환됨
+        if(todoDb !=null){
             println("Todo 데이터베이스 오픈됨");
-        }
-        dbCategory = openOrCreateDatabase(databaseCategory, MODE_PRIVATE,null) ; //보안때문에 요즘은 대부분 PRIVATE사용, SQLiteDatabase객체가 반환됨
-        if(dbCategory !=null){
-            println("Category 데이터베이스 오픈됨");
         }
     }
 
-    public void createTable(String tableName1, String tableName2){
+    /* Todo table, Category table 생성 */
+    public void createTable(){
         println("createTodoTable() 호출됨.");
-
-        if(dbTodo!= null) {
-            String sql = "create table if not exists " + tableName1 + "(_id integer PRIMARY KEY autoincrement," +
-                    " todo text, date datetime, category text, state integer)";
-            dbTodo.execSQL(sql);
-
+        todoDb.execSQL("PRAGMA foreign_keys = ON");
+        if(todoDb!= null) {
+            String sqlTodo = "create table if not exists Todo(_id integer PRIMARY KEY autoincrement," +
+                    " todo text, date datetime, state integer, category text, FOREIGN KEY(category) REFERENCES Category(categoryName) ON DELETE RESTRICT)";
+            todoDb.execSQL(sqlTodo);
             println("Todo 테이블 생성됨.");
-        } else {
-            println("데이터베이스를 먼저 오픈하세요");
-        }
 
-        if(dbCategory!= null) {
-            String sql = "create table if not exists " + tableName2 + "(_id integer PRIMARY KEY autoincrement," +
-                    " category text)";
-            dbCategory.execSQL(sql);
-
+            String sqlCategory = "create table if not exists Category(categoryName text PRIMARY KEY, color text)";
+            todoDb.execSQL(sqlCategory);
             println("Category 테이블 생성됨.");
         } else {
             println("데이터베이스를 먼저 오픈하세요");
         }
     }
 
-    public void insertTodoData(String todo, Date date, String category, int state){
+    /* Todo talbe에 Data 저장 */
+    public void insertTodoData(String todo, Date date, int state, String category){
         println("insertTodoData() 호출됨.");
 
-        if(dbTodo != null && todo.compareTo("") != 0
-                && category.compareTo("") != 0
-                && date.toString().compareTo("") != 0){
-            String sql = "insert into Todo(todo, date, category, state) values(?, ?, ?, ?)";
-            Object[] params = {todo, date, category, state};
-            dbTodo.execSQL(sql, params);//이런식으로 두번쨰 파라미터로 이런식으로 객체를 전달하면 sql문의 ?를 이 params에 있는 데이터를 물음표를 대체해준다.
+        if(todoDb != null && !todo.equals("") && !category.equals("") && !date.toString().equals("")) {
+            String sql = "insert into Todo(todo, date, state, category) values(?, ?, ?, ?)";
+            Object[] params = {todo, date, state, category};
+            todoDb.execSQL(sql, params);
             println("Todo 데이터 추가함");
         } else {
             println("데이터베이스를 먼저 오픈하시오");
         }
     }
 
-    public void selectTodoData(String tableName){
-        println("selectTodoData() 호출됨.");
-        if(dbTodo != null){
-            String sql = "select todo, date, category, state from "+tableName;
-            Cursor cursor = dbTodo.rawQuery(sql, null);
+    /* Todo table에 저장된 Data들 조회 */
+    public List<String[]> getTodoData(){
+        println("getTodoData() 호출됨.");
+        List<String[]> todoList = new ArrayList<>();
+        if(todoDb != null){
+            String sql = "select todo, date, state, category, Category.color from Todo " +
+                    "inner join Category on Todo.category = Category.categoryName";
+            Cursor cursor = todoDb.rawQuery(sql, null);
             println("조회된 데이터개수 :" + cursor.getCount());
 
-            for( int i = 0; i< cursor.getCount(); i++){
-                cursor.moveToNext();//다음 레코드로 넘어간다.
+            while (cursor.moveToNext()){
                 String todo = cursor.getString(0);
                 Date date = Date.valueOf(cursor.getString(1));
-                String category = cursor.getString(2);
-                int state = cursor.getInt(3);
-                println("#" + i + " -> " + todo + ", " + date + ", " + category + "," + state );
+                int state = cursor.getInt(2);
+                String category = cursor.getString(3);
+                String color = cursor.getString(4);
+                String[] todoData = {todo, date.toString(), String.valueOf(state), category, color};
+                todoList.add(todoData);
             }
             cursor.close();
+            return todoList;
         }
+        return null;
     }
-    public void insertCategoryData(String category){
-        println("insertCategoryData() 호출됨.");
-        if(dbCategory != null && category.compareTo("") != 0
-                && isExistCategoryData("Category", category)){
-            String sql = "insert into Category(category) values(?)";
-            Object[] params = {category};
-            dbCategory.execSQL(sql, params);//이런식으로 두번쨰 파라미터로 이런식으로 객체를 전달하면 sql문의 ?를 이 params에 있는 데이터를 물음표를 대체해준다.
-            println("데이터 추가함");
+
+    /* 전달받은 Data들로 Todo table 수정 */
+    public void updateTodoData(String todo, Date date, int state, String category) {
+        println("updateTodoData() 호출됨.");
+
+        if(todoDb != null && !todo.equals("") && !category.equals("") && !date.toString().equals("")) {
+            String sql = "update Todo set todo = ?, date = ?, state = ?, category = ?";
+            Object[] params = {todo, date, state, category};
+            todoDb.execSQL(sql, params);
+            println("Todo 데이터 수정함");
         } else {
-            println("데이터베이스를 먼저 오픈하시오");
-            println("데이터베이스에 이미 존재합니");
+            println("수정할 데이터를 입력하세요");
         }
     }
 
-    public int getCategoryCount(String tableName){
-        println("getCategoryCount() 호출됨.");
-        if(dbCategory != null){
-            String sql = "select category from "+tableName;
-            Cursor cursor = dbCategory.rawQuery(sql, null);
-            int cnt = cursor.getCount();
-            cursor.close();
-            return cnt;
-        }
-        return 0;
-    }
-    public boolean isExistCategoryData(String tableName, String findCategory){
-        println("isExistCategoryData() 호출됨.");
-        if(dbCategory != null){
-            String sql = "select category from "+tableName;
-            Cursor cursor = dbCategory.rawQuery(sql, null);
-            println("조회된 데이터개수 :" + cursor.getCount());
-
-            for( int i = 0; i< cursor.getCount(); i++){
-                cursor.moveToNext();//다음 레코드로 넘어간다.
-                String category = cursor.getString(0);
-                if(category.compareTo(findCategory) == 0) {
-                    return false;
-                }
+    /* Todo table에서 전달받은 todo로 데이터 삭제 */
+    public void deleteTodoData(String todo){
+        println("delteTodoData() 호출됨.");
+        try {
+            if(todoDb != null && !todo.equals("")){
+                String sql = "delete from Todo where todo = ?";
+                Object[] params = {todo};
+                todoDb.execSQL(sql, params);
+                println("데이터 삭제함");
             }
-            cursor.close();
+        } catch (Exception e) {
+            println(e.toString());
         }
-        return true;
     }
 
-    public List<String> getCategoryData(String tableName){
+//    public void selectTodoData(){
+//        println("selectTodoData() 호출됨.");
+//        if(todoDb != null){
+//            String sql = "select todo, date, state, category, Category.color from Todo " +
+//                    "inner join Category on Todo.category = Category.categoryName";
+//            Cursor cursor = todoDb.rawQuery(sql, null);
+//            println("조회된 데이터개수 :" + cursor.getCount());
+//            int i = 0;
+//            while (cursor.moveToNext()){
+//                i++;
+//                String todo = cursor.getString(0);
+//                Date date = Date.valueOf(cursor.getString(1));
+//                int state = cursor.getInt(2);
+//                String category = cursor.getString(3);
+//                String color = cursor.getString(4);
+//                println("#" + i + " -> " + todo + ", " + date + ", " + category + "," + state + "," + color);
+//            }
+//            cursor.close();
+//        }
+//    }
+
+//    public boolean isTodoExistCategory(String findCategory){
+//        println("selectTodoData() 호출됨.");
+//        if(todoDb != null){
+//            String sql = "select category from Todo";
+//            Cursor cursor = todoDb.rawQuery(sql, null);
+//            println("조회된 데이터개수 :" + cursor.getCount());
+//
+//            while (cursor.moveToNext()){
+//                String category = cursor.getString(0);
+//                if(category.equals(findCategory)) {
+//                    return true;
+//                }
+//            }
+//            cursor.close();
+//        }
+//        return false;
+//    }
+    /* Category table에 Data 저장 */
+    public void insertCategoryData(String category, String color){
+        println("insertCategoryData() 호출됨.");
+        try {
+            if(todoDb != null && !category.equals("")){
+                String sql = "insert into Category(categoryName, color) values(?, ?)";
+                Object[] params = {category, color};
+                todoDb.execSQL(sql, params);
+                println("데이터 추가함");
+            }
+        } catch (Exception e) {
+            println(e.toString());
+        }
+
+    }
+
+//    public int getCategoryCount(){
+//        println("getCategoryCount() 호출됨.");
+//        if(todoDb != null){
+//            String sql = "select categoryName from Category";
+//            Cursor cursor = todoDb.rawQuery(sql, null);
+//            int cnt = cursor.getCount();
+//            cursor.close();
+//            return cnt;
+//        }
+//        return 0;
+//    }
+
+//    public boolean isExistCategoryData(String findCategory){
+//        println("isExistCategoryData() 호출됨.");
+//        if(todoDb != null){
+//            String sql = "select categoryName from Category";
+//            Cursor cursor = todoDb.rawQuery(sql, null);
+//            println("조회된 데이터개수 :" + cursor.getCount());
+//
+//            while(cursor.moveToNext()) {
+//                String category = cursor.getString(0);
+//                if(category.equals(findCategory)) {
+//                    return false;
+//                }
+//            }
+//            cursor.close();
+//        }
+//        return true;
+//    }
+
+    /* Category table에 저장된 Data들 조회 */
+    public List<String[]> getCategoryData(){
         println("getCategoryData() 호출됨.");
-        List<String> categoryList = new ArrayList<>();
-        if(dbCategory != null){
-            String sql = "select category from "+tableName;
-            Cursor cursor = dbCategory.rawQuery(sql, null);
+        List<String[]> categoryList = new ArrayList<>();
+        if(todoDb != null){
+            String sql = "select categoryName, color from Category";
+            Cursor cursor = todoDb.rawQuery(sql, null);
             println("조회된 데이터개수 :" + cursor.getCount());
 
-            for( int i = 0; i< cursor.getCount(); i++){
-                cursor.moveToNext();//다음 레코드로 넘어간다.
+            while(cursor.moveToNext()) {
                 String category = cursor.getString(0);
-                categoryList.add(category);
+                String color = cursor.getString(1);
+                String[] categoryData = {category, color};
+                categoryList.add(categoryData);
             }
             cursor.close();
             return categoryList;
@@ -197,16 +260,18 @@ public class MainActivity extends AppCompatActivity {
         return null;
     }
 
+    /* Category table에서 전달받은 category를 삭제 */
     public void deleteCategoryData(String category){
         println("delteCategoryData() 호출됨.");
-        if(dbCategory != null && category.compareTo("") != 0){
-            String sql = "delete from Category where category = ?";
-            Object[] params = {category};
-            dbCategory.execSQL(sql, params);//이런식으로 두번쨰 파라미터로 이런식으로 객체를 전달하면 sql문의 ?를 이 params에 있는 데이터를 물음표를 대체해준다.
-            println("데이터 삭제함");
-        } else {
-            println("데이터베이스를 먼저 오픈하시오");
-            println("데이터베이스에 없습니다");
+        try {
+            if(todoDb != null && !category.equals("")){
+                String sql = "delete from Category where categoryName = ?";
+                Object[] params = {category};
+                todoDb.execSQL(sql, params);
+                println("데이터 삭제함");
+            }
+        } catch (Exception e) {
+            println(e.toString());
         }
     }
 
