43a1f7ae199d930e482d63ea73a55d2a175e1dd7
DevDanielNAM <danielnam426@gmail.com>
2023-11-29 23:54:56 +0900

    color picker 구현, 카테고리 버튼 연속적 추가 및 삭제 시 버그 수정

--- Team_FunFun/app/src/main/java/com/example/team_funfun/EditTodoFragment.java
+++ Team_FunFun/app/src/main/java/com/example/team_funfun/EditTodoFragment.java
@@ -5,6 +5,7 @@ import android.app.DatePickerDialog;
 import android.content.DialogInterface;
 import android.graphics.Color;
 import android.graphics.Typeface;
+import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
@@ -15,6 +16,7 @@ import androidx.core.content.ContextCompat;
 import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,6 +27,7 @@ import android.widget.GridLayout;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.sql.Date;
 import java.util.Calendar;
@@ -32,6 +35,8 @@ import java.util.List;
 
 public class EditTodoFragment extends Fragment {
     private GridLayout categoryContainer;
+    private AppCompatButton clickedButton = null;
+    private AppCompatButton clickedColorButton = null;
     Button editTodo;
     Button addCategory;
     Button deleteCategory;
@@ -39,12 +44,13 @@ public class EditTodoFragment extends Fragment {
     EditText dateInput;
     EditText categoryInput;
     ImageButton colorPicker;
-    HomeFragment homeFragment;
+    CategoryFragment categoryFragment;
     String clickedCategory = "";
     String content;
     String date;
     String category;
     int id;
+    String hexCodeColor = "#FFBF65";
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -52,7 +58,6 @@ public class EditTodoFragment extends Fragment {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_edit_todo, container, false);
 
         MainActivity mainActivity = (MainActivity)getActivity();
-        assert mainActivity != null;
 
         Bundle args = getArguments();
         if (args != null) {
@@ -62,7 +67,7 @@ public class EditTodoFragment extends Fragment {
             id = args.getInt("id", 0);
         }
 
-        homeFragment = new HomeFragment();
+        categoryFragment = new CategoryFragment();
 
         editTodo = rootView.findViewById(R.id.editTodo);
         addCategory = rootView.findViewById(R.id.addCategory);
@@ -96,10 +101,21 @@ public class EditTodoFragment extends Fragment {
                         }
                     });
                     denyDialog.show();
+                } else if(categoryInput.getText().toString().length() > 6) {
+                    denyDialog.setTitle("글자수 초과!");
+                    denyDialog.setMessage("글자수는 6자리까지 가능합니다!");
+                    denyDialog.setNegativeButton("확인", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+                    denyDialog.show();
                 } else if(!categoryInput.getText().toString().equals("")) {
-                    mainActivity.insertCategoryData(categoryInput.getText().toString(),"#BFFF4141");
+                    mainActivity.insertCategoryData(categoryInput.getText().toString(), hexCodeColor);
                     categoryInput.setText("");
                     renderCategoryBtn(mainActivity);
+                    Toast.makeText(getContext(), "카테고리가 추가되었습니다!", Toast.LENGTH_LONG).show();
                 } else {
                     denyDialog.setTitle("미입력!");
                     denyDialog.setMessage("추가할 카테고리를 입력해주세요!");
@@ -120,6 +136,7 @@ public class EditTodoFragment extends Fragment {
                 if(!clickedCategory.equals("")) {
                     mainActivity.deleteCategoryData(clickedCategory);
                     renderCategoryBtn(mainActivity);
+                    Toast.makeText(getContext(), "카테고리가 삭제되었습니다!", Toast.LENGTH_LONG).show();
                 } else {
                     AlertDialog.Builder denyDialog = new AlertDialog.Builder(getContext());
                     denyDialog.setTitle("미선택!");
@@ -186,8 +203,9 @@ public class EditTodoFragment extends Fragment {
                                         clickedCategory,
                                         id);
                             }
-                            mainActivity.getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+                            mainActivity.getSupportFragmentManager().beginTransaction().replace(R.id.container, categoryFragment).commit();
                             dialog.dismiss();
+                            Toast.makeText(getContext(), "Todo가 수되었습니다!", Toast.LENGTH_LONG).show();
                         }
                     });
                     editDialog.show();
@@ -202,7 +220,32 @@ public class EditTodoFragment extends Fragment {
                 AlertDialog.Builder menu = new AlertDialog.Builder(getContext());
                 menu.setTitle("카테고리 색상 선택");
 
-                menu.setView(R.layout.color_picker);
+                View colorPickerView = LayoutInflater.from(getContext()).inflate(R.layout.color_picker, null);
+
+                /* color_picker.xml의 colorPicker1 ~ colorPicker9까지의 버튼에 onClickListener 추가 */
+                for (int i = 1; i <= 9; i++) {
+                    int buttonId = getResources().getIdentifier("colorPicker" + i, "id", requireContext().getPackageName());
+                    AppCompatButton colorButton = colorPickerView.findViewById(buttonId);
+                    colorButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            // 클릭된 버튼이 있으면 원래 텍스트로 돌리기
+                            if(clickedColorButton != null) {
+                                clickedColorButton.setText("");
+                            }
+
+                            // 현재 클릭된 버튼 설정
+                            clickedColorButton = colorButton;
+                            // 현재 클릭된 버튼에 텍스트 추가
+                            colorButton.setText("✓");
+
+                            onColorButtonClick(v);
+                            Toast.makeText(getContext(), "선택한 색상은 " + hexCodeColor, Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }
+
+                menu.setView(colorPickerView);
 
                 menu.setNegativeButton("취소", new DialogInterface.OnClickListener() {
                     @Override
@@ -214,6 +257,7 @@ public class EditTodoFragment extends Fragment {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         dialog.dismiss();
+                        Toast.makeText(getContext(), "색상이 선택되었습니다!", Toast.LENGTH_LONG).show();
                     }
                 });
 
@@ -222,6 +266,24 @@ public class EditTodoFragment extends Fragment {
         });
 
         return rootView;
+    }
+
+    public void onColorButtonClick(View view) {
+        Drawable background = view.getBackground();
+
+        int backgroundColor;
+
+        if (background instanceof ColorDrawable) {
+            backgroundColor = ((ColorDrawable) background).getColor();
+        } else if (background instanceof GradientDrawable) {
+            backgroundColor = ((GradientDrawable) background).getColor().getDefaultColor();
+        } else {
+            backgroundColor = Color.TRANSPARENT;
+        }
+
+        // 투명도 포함한 헥사코드로 변환
+        hexCodeColor = String.format("#%08X", backgroundColor);
+        System.out.println("Selected Color: " + hexCodeColor);
     }
 
     private void showCalendar(MainActivity mainActivity, TextView dateInput) {
@@ -246,13 +308,14 @@ public class EditTodoFragment extends Fragment {
         });
     }
 
-    private AppCompatButton clickedButton = null;
-    // 카테고리 버튼 생성
+    /* 카테고리 버튼 생성 */
     private void addCategoryBtn(String category, String color, MainActivity mainActivity) {
+        // 새로운 카테고리 버튼 생성
         AppCompatButton newCategoryBtn = new AppCompatButton(getContext());
         int textColor = getContrastColor(color);
         int clickedColor = adjustColor(Color.parseColor(color), 0.8f);
 
+        // 버튼 속성 설정
         Typeface typeface = ResourcesCompat.getFont(getContext(), R.font.ibm_plex_sans_kr_light);
         newCategoryBtn.setTypeface(typeface);
         newCategoryBtn.setText(category);
@@ -261,45 +324,23 @@ public class EditTodoFragment extends Fragment {
         newCategoryBtn.setHeight(30);
         newCategoryBtn.setAllCaps(false);
 
-        // 전달받은 color를 unclicked 상태의 버튼에 적용
-        Drawable unclickedDrawable = ContextCompat.getDrawable(getContext(), R.drawable.unclicked_category_button);
-        GradientDrawable gradientUnclickedDrawable = (GradientDrawable) unclickedDrawable;
-        if (gradientUnclickedDrawable != null) {
-            gradientUnclickedDrawable.setColor(Color.parseColor(color));
-        }
+        // 버튼 상태에 따른 Drawable 생성
+        Drawable unclickedDrawable = createCategoryButtonDrawable(color);
+        Drawable clickedDrawable = createCategoryButtonDrawable(String.format("#%08X", clickedColor));
 
-        // 전달받은 color를 clicked 상태의 버튼에 적용
-        Drawable clickedDrawable = ContextCompat.getDrawable(getContext(), R.drawable.clicked_category_button);
-        GradientDrawable gradientClickedDrawable = (GradientDrawable) clickedDrawable;
-        if (gradientClickedDrawable != null) {
-            gradientClickedDrawable.setColor(clickedColor);
-        }
-
+        // 초기에 unclicked Drawable을 배경으로 설정
         newCategoryBtn.setBackgroundDrawable(unclickedDrawable);
 
+        // 클릭 리스너 설정
         newCategoryBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // 클릭된 버튼이 있으면 원래 색상으로 돌리기
-                if (clickedButton != null) {
-                    Drawable unclickedDrawable = ContextCompat.getDrawable(getContext(), R.drawable.unclicked_category_button);
-                    GradientDrawable gradientUnclickedDrawable = (GradientDrawable) unclickedDrawable;
-                    if (gradientUnclickedDrawable != null) {
-                        gradientUnclickedDrawable.setColor(Color.parseColor(mainActivity.getCategoryColor(clickedButton.getText().toString())));
-                    }
-
-                    clickedButton.setBackgroundDrawable(unclickedDrawable);
-                }
-
-                // 현재 클릭된 버튼 설정
-                clickedButton = newCategoryBtn;
-
-                // 현재 클릭된 버튼을 clickedDrawable로 변경
-                newCategoryBtn.setBackgroundDrawable(clickedDrawable);
-                clickedCategory = newCategoryBtn.getText().toString();
+                // 클릭된 버튼 처리
+                handleCategoryButtonClick(newCategoryBtn, clickedDrawable, mainActivity);
             }
         });
 
+        // 레이아웃 파라미터 설정
         LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
                 ViewGroup.LayoutParams.WRAP_CONTENT,
                 ViewGroup.LayoutParams.WRAP_CONTENT);
@@ -307,7 +348,41 @@ public class EditTodoFragment extends Fragment {
         int marginInPixels = (int) getResources().getDimension(R.dimen.margin);
         layoutParams.setMargins(marginInPixels, marginInPixels, marginInPixels, marginInPixels);
 
+        // 버튼을 categoryContainer에 추가
         categoryContainer.addView(newCategoryBtn, layoutParams);
+    }
+
+    /* 카테고리 버튼 Drawable 생성 */
+    private Drawable createCategoryButtonDrawable(String color) {
+        // unclicked 상태의 Drawable을 가져옴
+        Drawable drawable = ContextCompat.getDrawable(getContext(), R.drawable.unclicked_category_button);
+
+        if (!TextUtils.isEmpty(color)) {  // 색상이 비어있지 않은 경우
+            // GradientDrawable로 변환하여 색상 설정
+            GradientDrawable gradientDrawable = (GradientDrawable) drawable;
+            if (gradientDrawable != null) {
+                gradientDrawable.setColor(Color.parseColor(color));
+            }
+        }
+
+        return drawable;
+    }
+
+    /* 카테고리 버튼 클릭 처리 */
+    private void handleCategoryButtonClick(AppCompatButton button, Drawable clickedDrawable, MainActivity mainActivity) {
+        // 이전에 클릭된 버튼이 있으면 원래 색상으로 돌림
+        if (clickedButton != null) {
+            Drawable unclickedDrawable = createCategoryButtonDrawable(mainActivity.getCategoryColor(clickedButton.getText().toString()));
+            clickedButton.setBackgroundDrawable(unclickedDrawable);
+        }
+
+        // 현재 클릭된 버튼 설정
+        clickedButton = button;
+
+        // 클릭된 상태의 Drawable로 변경
+        button.setBackgroundDrawable(clickedDrawable);
+        clickedCategory = button.getText().toString();
+        mainActivity.getCategoryColor(clickedCategory);
     }
 
     private int adjustColor(int color, float factor) {
@@ -328,7 +403,7 @@ public class EditTodoFragment extends Fragment {
         return luminance > 0.5 ? Color.BLACK : Color.WHITE;
     }
 
-    // DB에서 카테고리 항목을 조회해서 카테고리 버튼을 렌더
+    /* DB에서 카테고리 항목을 조회해서 카테고리 버튼을 렌더 */
     private void renderCategoryBtn(MainActivity mainActivity) {
         categoryContainer.removeAllViews();
         List<String[]> getCategoryList = mainActivity.getCategoryData();
